ebc9088d341a031fbfe1d7c8dfca61efe830cb4f
Raihan Ali <trueraihan@gmail.com>
2024-10-04 20:12:03 +0600

    config: font family and container setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,36 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./helpers/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1200px",
+      "2xl": "1634px",
+    },
+
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1rem",
+        lg: "1rem",
+        xl: "1rem",
+        "2xl": "1rem", //1618 + 16 px = 1634
+      },
+    },
+    fontFamily: {
+      montserrat: ["var(--font-montserrat)"],
+      inter: ["var(--font-inter)"],
+      "act-of-rejection": ["var(--font-act-of-rejection)"],
+    },
+    extend: {
+      maxWidth: {
+        "1684px": "1684px",
+      },
+    },
   },
   plugins: [],
 };
