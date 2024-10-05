4515133d593d7535397d85e93294e70289f3f02b
Raihan Ali <trueraihan@gmail.com>
2024-10-05 12:48:54 +0600

    feat: styles text added

--- components/home/hero/index.jsx
+++ components/home/hero/index.jsx
@@ -4,6 +4,11 @@ const Hero = () => {
   return (
     <div className="bg-red">
       <Navbar />
+      <div className="container mx-auto">
+        <p className="font-act-of-rejection text-white/40 text-140px leading-168px">
+          future looks brights!
+        </p>
+      </div>
     </div>
   );
 };
